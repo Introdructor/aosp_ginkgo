024e0c4aa8f5fbd3ad4e8bd113f13e364a199b8b
Introdructor <yashkachave7@gmail.com>
2020-06-13 11:54:30 +0000

    ginkgo:sakurablossomed

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2019 The Aosp Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_ginkgo.mk
+    $(LOCAL_DIR)/lineage_ginkgo.mk
